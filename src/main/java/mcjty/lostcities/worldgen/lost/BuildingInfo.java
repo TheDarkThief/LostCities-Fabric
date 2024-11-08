c3478788ac2e88fe36ee8fb8b581381f46c4aa28
McJty <mcjty1@gmail.com>
2024-11-08 06:55:26 +0100

    - Fixed maxfloors not being honored completely. It was possible for buildings to go to maxfloors+1 - Multibuildings will now override structures no matter what. This had to be done because otherwise multibuildings could sometimes generate partially

--- src/main/java/mcjty/lostcities/worldgen/lost/BuildingInfo.java
+++ src/main/java/mcjty/lostcities/worldgen/lost/BuildingInfo.java
@@ -762,8 +762,8 @@ public class BuildingInfo implements ILostChunkInfo {
             int maxfloors = getMaxfloors(cs);
             int f = profile.BUILDING_MINFLOORS + rand.nextInt((int) (profile.BUILDING_MINFLOORS_CHANCE + (cityFactor + .1f) * (profile.BUILDING_MAXFLOORS_CHANCE - profile.BUILDING_MINFLOORS_CHANCE)));
             f++;
-            if (f > maxfloors + 1) {
-                f = maxfloors + 1;
+            if (f > maxfloors) {
+                f = maxfloors;
             }
             int minfloors = getMinfloors(cs);
             if (f < minfloors) {
